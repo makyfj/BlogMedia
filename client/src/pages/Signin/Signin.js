f4a13d26a824ebc9fc4262fc622d4c737295c3ad
Maky <franklinjaradev@gmail.com>
2021-12-27 14:23:31 -0500

    chore: adding signup mutation

--- client/src/pages/Signin/Signin.js
+++ client/src/pages/Signin/Signin.js
@@ -1,15 +1,52 @@
 import React, { useState, useEffect } from "react";
-
+import { useHistory } from "react-router";
+import { gql, useMutation } from "@apollo/client";
 import { Form } from "react-bootstrap";
 import Button from "@restart/ui/esm/Button";
 
+const SIGNIN = gql`
+  mutation Signin($email: String!, $password: String!) {
+    signin(credentials: { password: $password, email: $email }) {
+      userErrors {
+        message
+      }
+      token
+    }
+  }
+`;
+
 export default function Signin() {
+  const [signin, { data, loading }] = useMutation(SIGNIN);
+
+  const history = useHistory();
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleClick = () => {};
+  const handleClick = () => {
+    signin({
+      variables: {
+        email,
+        password,
+      },
+    });
+  };
 
+  console.log(data);
   const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (data) {
+      if (data.signin.userErrors.length) {
+        setError(data.signin.userErrors[0].message);
+      }
+
+      if (data.signin.token) {
+        localStorage.setItem("token", data.signin.token);
+        setError(null);
+      }
+    }
+  }, [data]);
 
   return (
     <div>
