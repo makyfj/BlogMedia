6041ddaf82695b84e445d219054d9f403d757e20
Maky <franklinjaradev@gmail.com>
2021-12-26 19:23:37 -0500

    chore: adding postUpdate to mutation

--- server/src/resolvers/Mutation.ts
+++ server/src/resolvers/Mutation.ts
@@ -1,24 +1,28 @@
 import { Context } from "../index";
-import { Post } from "@prisma/client";
+import { Post, Prisma } from "@prisma/client";
 
-interface PostCreateArgs {
-  title: string;
-  content: string;
+interface PostArgs {
+  post: {
+    title?: string;
+    content?: string;
+  };
 }
 
 interface PostPayloadType {
   userErrors: {
     message: string;
   }[];
-  post: Post | null;
+  post: Post | Prisma.Prisma__PostClient<Post> | null;
 }
 
 export const Mutation = {
   postCreate: async (
     _: any,
-    { title, content }: PostCreateArgs,
+    { post }: PostArgs,
     { prisma }: Context
   ): Promise<PostPayloadType> => {
+    const { title, content } = post;
+
     if (!title || !content) {
       return {
         userErrors: [
@@ -30,17 +34,75 @@ export const Mutation = {
       };
     }
 
-    const post = await prisma.post.create({
-      data: {
-        title,
-        content,
-        userId: "ckxnvldnh00725kcmn9y5ge34",
+    return {
+      userErrors: [],
+      post: prisma.post.create({
+        data: {
+          title,
+          content,
+          userId: "ckxnvldnh00725kcmn9y5ge34",
+        },
+      }),
+    };
+  },
+
+  postUpdate: async (
+    _: any,
+    { post, postId }: { postId: string; post: PostArgs["post"] },
+    { prisma }: Context
+  ): Promise<PostPayloadType> => {
+    const { title, content } = post;
+
+    if (!title && !content) {
+      return {
+        userErrors: [
+          {
+            message: "Need to have at least one field to update",
+          },
+        ],
+        post: null,
+      };
+    }
+
+    // Checks if post exists
+    const existingPost = await prisma.post.findUnique({
+      where: {
+        id: postId,
       },
     });
 
+    if (!existingPost) {
+      return {
+        userErrors: [
+          {
+            message: "Post does not exist",
+          },
+        ],
+        post: null,
+      };
+    }
+
+    let payloadToUpdate = {
+      title,
+      content,
+    };
+
+    // If title is not provided, we don't want to update it
+    if (!title) delete payloadToUpdate.title;
+
+    // If content is not provided, we don't want to update it
+    if (!content) delete payloadToUpdate.content;
+
     return {
       userErrors: [],
-      post,
+      post: prisma.post.update({
+        where: {
+          id: postId,
+        },
+        data: {
+          ...payloadToUpdate,
+        },
+      }),
     };
   },
 };
