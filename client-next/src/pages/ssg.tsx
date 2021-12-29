e2079ba327de7b9518d1492093c46126f5b7d7cd
Maky <franklinjaradev@gmail.com>
2021-12-29 18:06:05 -0500

    remove: ssg and ssr code commented for production

--- client-next/src/pages/ssg.tsx
+++ client-next/src/pages/ssg.tsx
@@ -1,68 +1,76 @@
-import { gql, useQuery } from "@apollo/client";
-import { GetStaticPropsContext } from "next";
-import { initializeApollo, addApolloState } from "../lib/apolloClient";
+import React from "react";
 
-interface Country {
-  name: string;
-  code: string;
-  emoji: string;
-}
+const SSGPage = () => {
+  return <div></div>;
+};
 
-const GET_COUNTRIES = gql`
-  query CountriesQuery {
-    countries {
-      name
-      code
-      emoji
-    }
-  }
-`;
+export default SSGPage;
 
-export default function SSG() {
-  const { loading, error, data } = useQuery(GET_COUNTRIES);
-  const countries = data?.countries.map((country: Country) => country) || [];
-  const haveCountries = Boolean(countries.length);
+// import { gql, useQuery } from "@apollo/client";
+// import { GetStaticPropsContext } from "next";
+// import { initializeApollo, addApolloState } from "../lib/apolloClient";
 
-  return (
-    <>
-      <h1>SSG Page</h1>
-      {loading ? (
-        <p>Loading...</p>
-      ) : error ? (
-        <p>An error has occurred.</p>
-      ) : !haveCountries ? (
-        <p>No posts found.</p>
-      ) : (
-        countries.map((country: Country, id: number) => {
-          return (
-            <article
-              key={id}
-              style={{
-                border: "2px solid #eee",
-                padding: "1rem",
-                marginBottom: "1rem",
-                borderRadius: "10px",
-              }}
-            >
-              <h2>{country.name}</h2>
-              <h3>{country.code}</h3>
-              <h3>{country.emoji}</h3>
-            </article>
-          );
-        })
-      )}
-    </>
-  );
-}
+// interface Country {
+//   name: string;
+//   code: string;
+//   emoji: string;
+// }
 
-export async function getStaticProps(context: GetStaticPropsContext) {
-  const apolloClient = initializeApollo();
+// const GET_COUNTRIES = gql`
+//   query CountriesQuery {
+//     countries {
+//       name
+//       code
+//       emoji
+//     }
+//   }
+// `;
 
-  await apolloClient.query({
-    query: GET_COUNTRIES,
-  });
+// export default function SSG() {
+//   const { loading, error, data } = useQuery(GET_COUNTRIES);
+//   const countries = data?.countries.map((country: Country) => country) || [];
+//   const haveCountries = Boolean(countries.length);
 
-  return addApolloState(apolloClient, {
-    props: {},
-  });
-}
+//   return (
+//     <>
+//       <h1>SSG Page</h1>
+//       {loading ? (
+//         <p>Loading...</p>
+//       ) : error ? (
+//         <p>An error has occurred.</p>
+//       ) : !haveCountries ? (
+//         <p>No posts found.</p>
+//       ) : (
+//         countries.map((country: Country, id: number) => {
+//           return (
+//             <article
+//               key={id}
+//               style={{
+//                 border: "2px solid #eee",
+//                 padding: "1rem",
+//                 marginBottom: "1rem",
+//                 borderRadius: "10px",
+//               }}
+//             >
+//               <h2>{country.name}</h2>
+//               <h3>{country.code}</h3>
+//               <h3>{country.emoji}</h3>
+//             </article>
+//           );
+//         })
+//       )}
+//     </>
+//   );
+// }
+
+// export async function getStaticProps(context: GetStaticPropsContext) {
+//   const apolloClient = initializeApollo();
+
+//   await apolloClient.query({
+//     query: GET_COUNTRIES,
+//   });
+
+//   return addApolloState(apolloClient, {
+//     props: {},
+//   });
+// }
