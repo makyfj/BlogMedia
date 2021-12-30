d688997afd9abb2146f116d6f96abffa067f4649
Franklin <65879341+makyfj@users.noreply.github.com>
2021-12-29 20:17:28 -0500

    Delete client-next directory

--- client-next/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  env: {
-    API_URL: "https://blog-media-777.herokuapp.com/graphql",
-  },
-};
--- client-next/src/pages/index.tsx
+++
@@ -1,7 +0,0 @@
-import type { NextPage } from "next";
-
-const Home: NextPage = () => {
-  return <div className="text-center font-bold">Hello</div>;
-};
-
-export default Home;
